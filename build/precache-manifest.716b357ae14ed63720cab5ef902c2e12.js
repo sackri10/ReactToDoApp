1eb276fdb8340bca2ce68bf3c98400e12fdab313
Datta Sai Krishna <sackri10@gmail.com>
2020-09-20 18:52:10 +0530

    Build Files Upload

---
+++ build/precache-manifest.716b357ae14ed63720cab5ef902c2e12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de76ef7c286cdbb9d4d5cba95354b379",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96eef285280c27d616b9",
+    "url": "/static/css/main.07c7b662.chunk.css"
+  },
+  {
+    "revision": "89a2f16e3407de0c8418",
+    "url": "/static/js/2.b08739e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b08739e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96eef285280c27d616b9",
+    "url": "/static/js/main.f0d365b4.chunk.js"
+  },
+  {
+    "revision": "10545e6b10910a0b9f12",
+    "url": "/static/js/runtime-main.025be44c.js"
+  }
+]);
